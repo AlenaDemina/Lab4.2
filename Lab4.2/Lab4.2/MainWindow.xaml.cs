ac8fe27c03d48a02f1e2e0b3e48971078e248da4
Alena Demina <alenademka02@gmail.com>
2021-12-07 13:51:10 +0700

    create plys and minus

---
+++ Lab4.2/Lab4.2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab4._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _1_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(A.Text);
+            double y = double.Parse(B.Text);
+            double r = x + y;
+            v.Content = r;
+        }
+
+        private void _2_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(A.Text);
+            double y = double.Parse(B.Text);
+            double r = x - y;
+            v.Content = r;
+        }
+
+    }
+}
